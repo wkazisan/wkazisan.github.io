8a5f32fd5b474ff5269d56fa26721c9ab19f6d0c
wkazisan <wkazisan@users.noreply.github.com>
2026-03-03 07:46:40 +0000

    deploy: 0628ecdf6358704d335b78452be5d6d470d7527c

--- component---cache-caches-gatsby-plugin-offline-app-shell-js-86b5ddc51e51f54ba180.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkwkazisan_site=self.webpackChunkwkazisan_site||[]).push([[681],{8826:function(e,t,n){var u=n(4994);t.__esModule=!0,t.default=void 0;var a=u(n(6221)),r=u(n(6540)),l=function(e){function t(){return e.apply(this,arguments)||this}return(0,a.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component),f=l;t.default=f}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-86b5ddc51e51f54ba180.js.map
